5661890a1e386e76c72b05f59399f939b5e8c104
JN0462 <115679916+JN0462@users.noreply.github.com>
2022-11-10 11:09:29 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+let x = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.lightLevel())
+})
+input.onButtonPressed(Button.B, function () {
+    x = input.lightLevel()
+    if (x < 50) {
+        led.setBrightness(255)
+    }
+    if (x >= 200) {
+        led.setBrightness(0)
+    }
+    if (x >= 100 && x < 200) {
+        led.setBrightness(64)
+    }
+    if (x < 255) {
+        led.setBrightness(255)
+    }
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
